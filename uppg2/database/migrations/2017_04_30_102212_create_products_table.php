b0a9ab30cb99572e474923ae0d06f2a741a4b44c
Stina Student <christofer.malmberg@gmail.com>
2017-04-30 15:56:17 +0200

    Added HUI

--- uppg2/database/migrations/2017_04_30_102212_create_products_table.php
+++ uppg2/database/migrations/2017_04_30_102212_create_products_table.php
@@ -6,14 +6,10 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateProductsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+
     public function up()
     {
-        Schema::create('products', function(Blueprint $table){
+        Schema::create('products', function (Blueprint $table){
           $table->increments('id');
           $table->text('title');
           $table->text('brand');
@@ -24,11 +20,7 @@ class CreateProductsTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
+
     public function down()
     {
         //
